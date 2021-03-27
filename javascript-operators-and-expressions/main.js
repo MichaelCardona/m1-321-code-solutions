24085a79227ba07691800703c8525d749059706e
MichaelCardona <michaelcx0624@gmail.com>
2021-03-27 13:01:16 -0700

    javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,47 @@
+var width = 60;
+var height = 20;
+var area = width * height;
+
+console.log('area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 30;
+var payment = 20;
+var change = payment - bill;
+
+console.log('Change:', change);
+console.log('typeof Change:', typeof change);
+
+var quizzes = 5;
+var midterm = 2;
+var final = 1;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('Grade:', grade);
+console.log('typeof Grade:', typeof grade);
+
+var firstName = 'Michael';
+var lastName = 'Cardona';
+var fullName = firstName + ' ' + lastName;
+
+console.log('Full Name:', fullName);
+console.log('typeof Full name:', typeof fullName);
+
+var pH = 5;
+var acidic = 7;
+var isAcidic = pH < acidic;
+
+console.log('Is Acidic:', isAcidic);
+console.log('typeof Is Acidic:', typeof isAcidic);
+
+var headCount = 100;
+var _300 = 300;
+var isSparta = headCount === _300;
+
+console.log('Is Sparta:', isSparta);
+console.log('typeof Is Sparta:', typeof isSparta);
+
+var motto = fullName;
+
+console.log(motto + ' ' + 'is' + ' ' + 'a' + ' ' + 'GOAT');
+console.log('typeof motto:', typeof motto);
