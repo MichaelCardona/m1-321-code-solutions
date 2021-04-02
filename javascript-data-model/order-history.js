0eb11a689cc66acb2a67c306623a43c0bd17a1a5
MichaelCardona <michaelcx0624@gmail.com>
2021-04-02 13:09:02 -0700

    updated JS-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -3,7 +3,10 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNum: '114-3941689-8772232',
     totalPrice: 34.00,
-    itemName: 'JavaScript for impatient programmer',
+    items: [{
+      type: 'book',
+      title: 'JavaScript for impatient programmer'
+    }],
     author: 'Rauschmayer, Dr. Axel',
     itemPrice: 31.55,
     dateOrder: 'August 4, 2020',
@@ -14,7 +17,10 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNum: '113-9984268-120257',
     totalPrice: 44.53,
-    itemName: 'The Timeless Way of Building',
+    items: [{
+      type: 'book',
+      title: 'The Timeless Way of Building'
+    }],
     author: 'Alexander, Christopher',
     itemPrice: 41.33,
     dateOrder: 'July 19, 2020',
@@ -25,7 +31,10 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNum: '114-2875557-9059409',
     totalPrice: 17.22,
-    itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for the Wii U,PC Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    items: [{
+      type: 'electronic',
+      title: 'Gamecube Controller Adapter.Super Smash Bros Switch Gamecube Adapter for the Wii U, PC Support Turbo and Vibration Features.No Driver and No Lag- Gamecube Adapter'
+    }],
     itemPrice: 15.98,
     dateOrder: 'July 4, 2020',
     deliverDate: 'July 7, 2020',
@@ -35,9 +44,15 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNum: '113-2883177-2648248',
     totalPrice: 138.93,
-    itemName1: 'GameCube Controller - Super Smash Bros. Edition(Nintendo Switch)',
+    items: [{
+      type: 'electronic',
+      title: 'GameCube Controller - Super Smash Bros. Edition(Nintendo Switch)'
+    },
+    {
+      type: 'book',
+      title: 'The Art of SQL'
+    }],
     itemPrice1: 94.95,
-    itemName2: 'The Art of SQL',
     author: 'Faroult, Stephanie',
     itemPrice2: 33.99,
     dateOrder: 'July 4, 2020',
