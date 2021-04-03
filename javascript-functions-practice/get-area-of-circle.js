f1c6f2617062a3ee62fab7da69e03aa4c56b6d4b
MichaelCardona <michaelcx0624@gmail.com>
2021-04-03 13:12:15 -0700

    JS functions practice

---
+++ javascript-functions-practice/get-area-of-circle.js
@@ -0,0 +1,5 @@
+/* exported getAreaOfCircle */
+function getAreaOfCircle(radius) {
+  var areaOfCirlce = Math.PI * (radius * radius);
+  return areaOfCirlce;
+}
---
+++ javascript-functions-practice/get-last-character.js
@@ -0,0 +1,6 @@
+/* exported getLastCharacter */
+function getLastCharacter(string) {
+  var laststring = string.length - 1;
+  var lastCharacter = string[laststring];
+  return lastCharacter;
+}
