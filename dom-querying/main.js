4b75456356d1945caf08845aa30e4a5ea6546794
MichaelCardona <michaelcx0624@gmail.com>
2021-04-15 20:09:45 -0700

    dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,22 @@
+console.log('hello, world');
+
+var header = document.querySelector('h1');
+console.log(header);
+
+console.dir(document.querySelector('h1'));
+
+var idExp = document.querySelector('#explanation');
+console.log(idExp);
+
+console.dir(document.querySelector('#explanation'));
+
+var classHint = document.querySelector('.hint');
+console.log(classHint);
+
+console.dir(document.querySelector('.hint'));
+
+var allPar = document.querySelector('p');
+console.log(allPar);
+
+var exLink = document.querySelector('.example-link');
+console.log(exLink);
